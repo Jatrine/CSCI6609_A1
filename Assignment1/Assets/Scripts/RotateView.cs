bb0847662865587a179be4a2807f196a4f0b2c84
Jatrine <jatrinezhang@gmail.com>
2023-10-07 09:48:40 -0300

    first commit

---
+++ Assignment1/Assets/Scripts/RotateView.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateView : MonoBehaviour
+{
+    Gyroscope gyro;
+    Vector3 invisibleCam;
+
+    void Start()
+    {
+        gyro = Input.gyro;
+        gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        invisibleCam = new Vector3 (0, (transform.eulerAngles - gyro.rotationRateUnbiased * Time.deltaTime * Mathf.Rad2Deg).y, 0);
+        if (!DestroyRock.selecting)
+        {
+            transform.eulerAngles = invisibleCam;
+        }
+    }
+}
